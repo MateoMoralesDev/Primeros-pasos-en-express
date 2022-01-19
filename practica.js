d7ce1b3b0482aaeee3ee2a16ab9b68faf30d5019
Mateo Morales <96091498+MateoMoralesDev@users.noreply.github.com>
2022-01-19 16:23:17 -0500

    Update practica.js

--- practica.js
+++ practica.js
@@ -19,5 +19,4 @@ Pasos guía / ayuda para la resolución:
 4) Agregar la ruta 404 y enviar el mensaje correspondiente.
 
 5) Agregar la ruta root (/) del home y enviar el archivo correspondiente.
-
-DEBES HACERLO EN EL ARCHIVO PRACTICA.JS PARA NO SPOILEARTE LA SOLUCION */
+ */
